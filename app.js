e6fadb5541cef3c4bee4edc56a123ef4c60a9951
David-jpg-lab <davidbabayan164@gmail.com>
2024-08-19 19:32:30 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-let num1 = Number(prompt("Enter the first number:"));
-let num2 = Number(prompt("Enter the second number:"));
-
-console.log("The sum is:", num1+num2) 
+function addTwoNumbers(num1,num2) {
+     num1 = Number(prompt("Enter the first number:"));
+     num2 = Number(prompt("Enter the second number:"));
+    console.log("The sum is:", num1 + num2);
+    
+  }
+  addTwoNumbers();
